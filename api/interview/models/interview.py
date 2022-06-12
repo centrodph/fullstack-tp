19b90f1458b33dd812c87812cb1b39c611770f99
Gerardo Perrucci <centrodph@gmail.com>
2022-06-11 21:47:17 -0300

    include candidate data on interview

--- api/interview/models/interview.py
+++ api/interview/models/interview.py
@@ -1,11 +1,19 @@
 from django.conf import settings
 from django.db import models
 
+from candidate.models.candidate import Candidate
+
 class Interview(models.Model):
     description = description = models.TextField()
     interviewer = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
+    )
+    candidate = models.ForeignKey(
+        Candidate,
+        related_name='candidates',
+        on_delete=models.PROTECT,
+        null=True,
     )
     code_access = models.CharField(max_length=2000, unique=True)
     date = models.DateTimeField()
--- api/interview/serializers/interview_serializer.py
+++ api/interview/serializers/interview_serializer.py
@@ -1,9 +1,11 @@
+from candidate.serializers.candidate import CandidateSerializer
 from interview.models.interview import Interview
 from rest_framework import serializers
 
 
 class InterviewSerializer(serializers.ModelSerializer):
     interviewer_username = serializers.CharField(source="interviewer.username", read_only=True)
+    candidate_data = CandidateSerializer(source="candidate", read_only=True)
     class Meta:
         model = Interview
         fields = '__all__'
